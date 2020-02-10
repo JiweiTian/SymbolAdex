28038ba9f6a372506b57f4b215e7150487025acf
dimitadi <dimitadi@student.ethz.ch>
2020-02-10 21:05:47 +0000

    Fix for requirements

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ cd ERAN
 git checkout 73f48315ae28da1a21f90ac9a6ef43338dd3190b
 sudo ./install.sh
 source gurobi_setup_path.sh
-pip3 install -r requirements.txt
 cd ..
 cp ./patches/clever_wolf_main.py ./ERAN/tf_verify/
 cp ./patches/clever_wolf.py ./ERAN/tf_verify/
