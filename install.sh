1d2639def95ad6743edfdc9a575e16ca920ac009
dimitadi <dimitadi@student.ethz.ch>
2020-02-12 15:17:57 +0000

    Added networks

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ cd ..
 cp ./patches/clever_wolf_main.py ./ERAN/tf_verify/
 cp ./patches/clever_wolf.py ./ERAN/tf_verify/
 cp ./patches/clever_pgd_generator.py ./ERAN/tf_verify/
+cp ./patches/nets/ ./ERAN/
 patch ./ERAN/tf_verify/deepzono_milp.py ./patches/patch_deepzono_milp.patch
 patch ./ERAN/tf_verify/read_net_file.py ./patches/patch_readnet_file.patch
 pip3 install -r requirements.txt
