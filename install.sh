d9e8245d537f71de94d9afdb2400ea96ef3cd376
dimitadi <dimitadi@student.ethz.ch>
2020-02-12 20:26:21 +0000

    fixed bash script

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 git clone https://github.com/eth-sri/ERAN.git
 cd ERAN
 git checkout 73f48315ae28da1a21f90ac9a6ef43338dd3190b
-sudo ./install.sh
-source gurobi_setup_path.sh
+bash -c  "./install.sh;source gurobi_setup_path.sh"
 cd ..
 cp ./patches/clever_wolf_main.py ./ERAN/tf_verify/
 cp ./patches/clever_wolf.py ./ERAN/tf_verify/
