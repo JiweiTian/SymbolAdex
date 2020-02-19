08d56d482da69d9d4bd7e1a89569e3bcaabf32d9
Dimitar <dimitadi@dlsrlxlarge.ethz.ch>
2020-02-19 14:12:48 +0100

    Fixed wrong script

--- patches/scripts/mortgage_lp.sh
+++ patches/scripts/mortgage_lp.sh
@@ -3,7 +3,7 @@ for file in ../data/mortgage/*.txt
 do
     #IMG=${file/\//} 
     #IMG=sed 's/\///g' "$file"
-    IMG=$( echo "$file" | sed 's/\/local\/home\/dimitadi\/symadex\/data\/mortgage\/spec//g' )
+    IMG=$( echo "$file" | sed 's/..\/data\/mortgage\/spec//g' )
     IMG=${IMG/.txt/} 
     for SEED in 10 20 30 40 50
     do
